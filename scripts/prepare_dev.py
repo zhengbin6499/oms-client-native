f50a29350432dbc2c06533773e1880b3fedac623
Qiu Jianlin <jianlin.qiu@intel.com>
2020-06-15 16:53:39 +0800

    Fix ia32 build errors for ffmpeg

--- scripts/prepare_dev.py
+++ scripts/prepare_dev.py
@@ -39,10 +39,6 @@ def _patch():
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0007-Fix-examples-path-error.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0009-Fix-compile-issue-for-linux-g-build.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
-  #  subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
-  #if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0006-Adjusted-jni_generator.py-to-fit-OWT-code-structure.patch')], shell=useShell, cwd=BASE_PATH)) != 0:
-  #  subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BASE_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0009-Export-WebRTC-symbols-on-iOS.patch')], shell=useShell, cwd=BUILD_PATH)) != 0:
     subprocess.call(['git', 'am', '--skip'], shell=useShell, cwd=BUILD_PATH)
   if (subprocess.call(['git', 'am', os.path.join(PATCH_PATH, '0011-libjpeg_turbo-fix-for-CVE-2018-20330-and-19664.patch')], shell=useShell, cwd=LIBJPEG_TURBO_PATH)) != 0:
